3330e8e5146cef5cc459682a3451cb2eb30bf545
Neonyi <150392916+LeNeonyi@users.noreply.github.com>
2025-02-24 14:53:16 +0100

    Added file for Intro Video removal

---
+++ DeleteOutlastTrialsIntro.ps1
@@ -0,0 +1,5 @@
+# Enter your path to your Outlast Trials Folder installation, you can right click right click the game on steam then do manage, browse local files to copy the path below in quotes
+$introLoc = "C:\Program Files (x86)\Steam\steamapps\common\The Outlast Trials"
+
+rm $introLoc\OPP\Content\Movies\TOT_ThirdParty.bk2
+rm $introLoc\OPP\Content\Movies\RedBarrelsLogo.bk2
